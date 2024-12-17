d32f2a95b4b4e0e12e4db295728429bd590e332c
vikasnayma <vikasnaymaofficial@gmail.com>
2024-12-17 12:17:25 +0530

    auth function updated

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -1,25 +1,18 @@
-import jwt from "jsonwebtoken";
+const jwt = require('jsonwebtoken');
+const secretKey = process.env.SECRET_KEY;
 
-const isAuthenticated = async (req, res, next) => {
-  try {
-    const token = req.cookies.token;
-    if (!token) {
-      return res.status(401).json({
-        message: "User not authenticated",
-        success: false,
-      });
-    }
-    const decode = await jwt.verify(token, process.env.SECRET_KEY);
-    if (!decode) {
-      return res.status(401).json({
-        message: "Invalid token",
-        success: false,
-      });
-    }
-    req.id = decode.userId;
+const isAuthenticated = (req, res, next) => {
+  
+   const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];  
+
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, secretKey, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
     next();
-  } catch (error) {
-    console.log(error);
-  }
+  });
 };
+
 export default isAuthenticated;
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -5,15 +5,5 @@ export const generateToken = (res, user, message) => {
     expiresIn: "1d",
   });
 
-  return res
-    .status(200)
-    .cookie("token", token, {
-      httpOnly: true,
-      sameSite: "strict",
-      maxAge: 24 * 60 * 60 * 1000, // 1 day
-    }).json({
-        success:true,
-        message,
-        user
-    });
+  return res.status(200).json({ token });
 };
